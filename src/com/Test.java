c4648a4c974139a727b3408998ffd4df4c3baa0e
William Nguyen <la.runeserver@gmail.com>
2013-04-01 16:56:56 -0500

    Added the Test class.

--- src/com/Test.java
+++ src/com/Test.java
@@ -1,11 +1,31 @@
 package com;
 
+import com.castlewood.services.world.actor.entity.Entity;
+import com.castlewood.services.world.actor.entity.Location;
+import com.castlewood.services.world.actor.entity.mobile.Direction;
+import com.castlewood.services.world.actor.entity.mobile.Directions;
+import com.castlewood.services.world.actor.entity.mobile.MobileEntity;
+import com.castlewood.services.world.actor.message.LocationChangeMessage;
+import com.castlewood.services.world.actor.message.LocationTranslationMessage;
+import com.castlewood.services.world.actor.message.PrimaryDirectionMessage;
+import com.castlewood.services.world.actor.message.SecondaryDirectionMessage;
+
 public class Test
 {
 
 	public static void main(final String... args)
 	{
-
+		Entity entity = new MobileEntity();
+		Location location = entity.acceptor(Location.class);
+		Directions directions = entity.acceptor(Directions.class);
+		entity.accept(new LocationChangeMessage(333, 444, 555));
+		entity.accept(new LocationTranslationMessage(1, 1, 1));
+		entity.accept(new PrimaryDirectionMessage(Direction.NORTH));
+		entity.accept(new SecondaryDirectionMessage(Direction.SOUTH));
+		entity.evalute();
+		System.out.println("Location: [" + location.x() + ", " + location.y()
+				+ ", " + location.height() + "]");
+		System.out.println("Directions: [" + directions.primary() + ", "
+				+ directions.secondary() + "]");
 	}
-
 }
---
+++ src/com/castlewood/services/world/actor/entity/mobile/DirectionsQueue.java
@@ -0,0 +1,14 @@
+package com.castlewood.services.world.actor.entity.mobile;
+
+/**
+ * The {@link DirectionsQueue} is used to depict the
+ * {@link Directions#primary()} and {@link Directions#secondary()}
+ * {@link Direction}s of a {@link MobileEntity}.
+ * 
+ * @author William Nguyen <L__A> <larevxpk@gmail.com>
+ * 
+ */
+public class DirectionsQueue
+{
+
+}
